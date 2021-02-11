e960c4632b5c79832b2752207c4dc54400d43ef1
Gabriela Mendes <contato@gabrielamendes.dev>
2021-02-10 23:01:08 -0300

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
-export default function App(){
-    return <View/>;
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#7159c1',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 32,
+        fontWeight: 'bold',
+
+    }
+});
+
+export default function App() {
+    return (
+        <>
+            <StatusBar barStyle="light-content"/>
+            <View style={styles.container}>
+                <Text style={styles.title}> Hello GO STACK</Text>
+            </View>
+        </>
+        );
 }
