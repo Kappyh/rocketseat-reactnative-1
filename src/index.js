4c29eed3e199b9a3f02ed0cf4b1f36068f3d77c6
Gabriela Mendes <contato@gabrielamendes.dev>
2021-02-11 22:41:53 -0300

    Adicionando botoes e listas

--- src/index.js
+++ src/index.js
@@ -1,29 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { TouchableOpacity, SafeAreaView, Text, StyleSheet, StatusBar, FlatList } from 'react-native';
+
+import api from './services/api';
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#7159c1',
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
     },
-    title: {
+    project: {
         color: '#FFF',
-        fontSize: 32,
+        fontSize: 20,
+    },
+    button: {
+        backgroundColor: '#fff',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
         fontWeight: 'bold',
-
+        fontSize: 16
     }
 });
 
 export default function App() {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        }
+        )
+    }, []);
+
+    async function handleAddProject() {
+        const response = await api.post('projects', { title: `Projeto novo ${Date.now()}`, owner: "Gabriela Mendes" });
+        setProjects([...projects, response.data]);
+    }
+
     return (
         <>
-            <StatusBar barStyle="light-content"/>
-            <View style={styles.container}>
-                <Text style={styles.title}> Hello GO STACK</Text>
-            </View>
+            <StatusBar barStyle="light-content" />
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={
+                        ({ item: project }) => (<Text style={styles.project}>{project.title}</Text>)
+                    }
+                />
+                <TouchableOpacity
+                    onPress={handleAddProject}
+                    activeOpacity={0.6}
+                    style={styles.button}>
+                    <Text style={styles.buttonText}>Adicionar Projecto</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
         </>
-        );
+    );
 }
